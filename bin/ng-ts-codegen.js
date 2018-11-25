938424721f3e2aa57185f772347de4e795fdd08d
cogidoo <cogidoo+github@gmail.com>
2018-11-25 21:53:52 +0100

    improve handling of the args and fix som docker-calls

--- bin/ng-ts-codegen.js
+++ bin/ng-ts-codegen.js
@@ -4,25 +4,47 @@ const argv = require('yargs').argv;
 const {exec} = require('child_process');
 const {resolve} = require('path');
 
+// Usage
 if (argv.help || argv.h) {
     console.log('[Usage]');
     console.log('openapi-typescript-angular-generator -i <openapi-spec> -o <output-destination> [-e <java|docker>]');
     process.exit(0);
 }
 
-const args = process.argv.slice(2);
+// input and output must be defined
+if (!argv.i) {
+    console.error('Please provide the input file with the openapi-specification.');
+    process.exit(1);
+}
+if (!argv.o) {
+    console.error('Please define an output-destination.');
+    process.exit(1);
+}
+
+// define the actual command
 let command;
+let isDocker = false;
 if (argv.e === 'docker') {
     command = 'docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli';
+    isDocker = true;
 } else {
     // default to java
     command = 'java -jar ' + resolve(__dirname, 'openapi-generator.jar');
 }
-command += ' generate -g=typescript-angular -t=src/mustache --additional-properties="ngVersion=7.0.0" --additional-properties="supportsES6=true"';
-if (args) {
-    command += ` ${args.join(' ')}`;
-}
 
+// join paramters to the command
+const args = [
+    'generate',
+    `-i ${argv.i}`,
+    `-o ${isDocker ? `/local/${argv.o}` : argv.o}`,
+    '-g=typescript-angular',
+    `-t=${isDocker ? '/local/node_modules/openapi-typescript-angular-generator/src/mustache' : resolve(__dirname, '../src/mustache')}`,
+    '--additional-properties="supportsES6=true"',
+    '--additional-properties="ngVersion=7.0.0"',
+];
+command += ` ${args.join(' ')}`;
+
+// execute
 const cmd = exec(command);
 cmd.stdout.pipe(process.stdout);
 cmd.stderr.pipe(process.stderr);
